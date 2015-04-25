aaf05196af8bb5fda4b3adb97db63c40b27f3ba4
Ethan Ferrari <ethan@onefiremedia.com>
2015-04-25 01:57:22 -0500

    more work with components, fixed a bug where octane.recompile did not pass a context argument

--- src/octane.js
+++ src/octane.js
@@ -346,8 +346,8 @@
 												return Octane;
 											},
 
-					recompile: 	function(){
-												return Compiler.run.apply(Compiler);
+					recompile: 	function(context){
+												return Compiler.run.apply(Compiler,[context]);
 											},
 
 					clearCompileCache: function(){
@@ -1163,43 +1163,122 @@
 
 				/* experimental */
 
-				Octane.createElement = function(type,properties,children){
-					var el = document.createElement(type);
-					_.extend(el,properties);
-					_.each(children,function(child){
-						if(_.isString(child)){
-							el.innerHTML = el.innerHTML+child;
-						} else if(_.isObject(child)){
-							el.appendChild(child);
-						}
+				function OctaneElement(t,a,c){
+					this.type 		= t || 'span';
+					this.attrs 		= a || {};
+					this.children = c || [];
+				}
+				_.extend(OctaneElement.prototype,{
+					render: 		function(){
+												var el = document.createElement(this.type);
+												_.each(this.children,function(child){
+													if(_.isString(child)){
+														el.innerHTML = el.innerHTML+child;
+													} else if(child instanceof OctaneElement){
+														el.appendChild(child.render());
+													} else if(child instanceof OctaneComponent){
+														Octane.render(child,el);
+													} else if(child instanceof HTMLElement){
+														el.appendChild(child);
+													}
+												});
+												if(this.attrs.styles){
+													_.each(this.attrs.styles,function(val,key){
+														el.style[key] = val;
+													});
+												}
+												_.extend(el,this.attrs);
+												return el;
+											}
+				});
+
+				Octane.createElement = function(type,attrs,children){
+					return new OctaneElement(type,attrs,children);
+				};
+
+
+
+
+
+				function OctaneComponent (params){
+
+					_.extend(this,params);
+					this.props = {};
+					this.state = {};
+					//Octane.guid(this);
+
+					this._renderedTo = null;
+
+					Octane.defineProp.call(this,{
+						update: 	function(){
+												if(this.hasMounted) Octane.render(this,this._renderedTo);
+											},
+						setState: function(obj){
+												_.extend(this.state,obj);
+												if(this.hasMounted) this.update();
+											}
 					});
-					return el;
-				};
 
-
-				Octane.render = function(Component,attrs,container){
-					var guid = octane.guid(Component);
-					_.extend(Component,attrs);
-					var newRender = Component.render();
-					var lastRender = container.querySelector('[octaneID="'+guid+'"]');
-
-					newRender.setAttribute('octaneID',guid);
-					lastRender ? container.replaceChild(newRender,lastRender) : container.appendChild(newRender);
-				};
-
-
-				function OctaneComponent(obj){
-					this.model = {};
-					this.collection = [];
-					this.render = function(){};
-					_.extend(this,obj);
+					Octane.defineGetter.call(this,'Node',
+											function(){
+												if(this._renderedTo){
+													return this._renderedTo.querySelector('[octane-id="'+this.octaneID+'"]');
+												}
+											});
 				}
 
+				_.extend(OctaneComponent.prototype,{
+					render: function(){},
+					getInitialState: function(){},
+					beforeMount: function(){
+						return Promise.resolve();
+					},
+					onMount: function(){},
+					onUnmount: function(){}
+				});
 
-				Octane.Component = function(obj){
-					return new OctaneComponent(obj);
+
+
+				Octane.Component = function(params){
+					var factory = function ComponentFactory(props){
+						var cmp = new OctaneComponent(params);
+						_.extend(cmp.props,props);
+						return cmp;
+					};
+					factory.create = function(props){
+						var cmp = new OctaneComponent(params);
+						_.extend(cmp.props,props);
+						return cmp;
+					};
+					return factory;
 				};
+
+
+				Octane.render = function(Component,container){
+
+					var newRender;
+					var lastRender = Component.Node;
+					if(lastRender){
+						newRender = Component.render();
+						if(newRender instanceof OctaneElement) newRender = newRender.render();
+						newRender.setAttribute('octane-id',Component.octaneID);
+						Component._renderedTo.replaceChild(newRender,lastRender);
+					} else {
+						Component._renderedTo = container;
+						Component.getInitialState();
+						Component.beforeMount().then(function(){
+							newRender = Component.render();
+							if(newRender instanceof OctaneElement) newRender = newRender.render();
+							Component.octaneID = octane.guid(newRender);
+							container.appendChild(newRender);
+							Component.hasMounted = true;
+							Component.onMount();
+						});
+					}
+				};
+
 				/* end experimental */
+
 
 
 				var ViewModel = Object.create(OctaneBase.prototype);
