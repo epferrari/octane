20d2226ef9adca476f1d6eb7af5b24afa77a2368
Ethan Ferrari <epferrari309@gmail.com>
2015-05-30 18:05:32 -0500

    removed grunt and gulp build tools

--- grunt/config/clean.js
+++
@@ -1,7 +0,0 @@
-/* config for Grunt clean */
-
-module.exports = {
-	build		:	['build','example-project/build'],
-	dist		:	['dist','example-project/dist'],
-	example : ['example-project/src/octane_core']
-},
