08871fa08ad41c9670588f18f1a52fedd6937d08
Ethan Ferrari <ethan@onefiremedia.com>
2015-06-01 18:45:51 -0500

    hinter didn't need to be a Factory

--- src/lib/uiMessages.js
+++ src/lib/uiMessages.js
@@ -2,28 +2,30 @@
 	var _           = require('lodash');
 	var OctaneModel = require('./OctaneModel.js');
 
-	var Hinter = OctaneModel.extend({
-		hint: function(){
+	var uiMessages = new OctaneModel();
+
+	uiMessages.hint =  function(){
 
 			var setObject,toUnset,timeout;
 			// handle key,value and {key:value}
 			if(_.isString(arguments[0])){
-					setObject = {};
-					setObject[arguments[0]] = arguments[1];
-					timeout = arguments[2];
+				setObject = {};
+				setObject[arguments[0]] = arguments[1];
+				timeout = arguments[2];
 			} else if(_.isObject(arguments[0])){
-					setObject = arguments[0];
-					timeout = arguments[1];
+				setObject = arguments[0];
+				timeout = arguments[1];
 			} else {
-					return {};
+				return {};
 			}
 			timeout || (timeout = 5000);
 
-			// automatically remove after 5 seconds or specified time
+			// automatically unset after 5 seconds || passed-in time
 			toUnset = Object.keys(setObject);
 			this._set(setObject);
 			this._unset(toUnset,{timeout:timeout});
-		}
-	});
+	};
 
-	module.exports = Hinter;
+	uiMessages.become('UiMessages');
+
+	module.exports = uiMessages;
